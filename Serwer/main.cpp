27a9eed1865407cfeb3783667afd7971644c5f8b
B-Kedziora <kedziora.bartosz@gmail.com>
2015-01-07 21:51:33 +0100

    Test entry point for serwer.

--- Serwer/main.cpp
+++ Serwer/main.cpp
@@ -1,9 +1,24 @@
 #include <QCoreApplication>
 #include <iostream>
-
-using namespace std;
+#include "qthread.h"
+#include <stdio.h>
+#include "../Commons/connectiondata.h"
 
 int main(int argc, char *argv[])
 {
+    stdout = freopen("server.log", "w", stdout);
 
+    if(argc == 2) {
+        try{
+            ConnectionData* cd = new ConnectionData(argv[1]);
+
+        } catch(exception e) {
+            cout<<"Invalid connection data "<<endl;
+        }
+    } else {
+        cout<<"Server cannot be created due to lack of connection details passed as an argument"<<endl;
+        return 1;
+    }
+
+    QThread::sleep(10000);
 }
